9d8de56850ce305441a4dd6c861567ba4a2cbe3f
shinriyo <shinriyo@gmail.com>
2012-12-03 09:53:38 +0900

    once finish

--- BooklogExtension/booklog.js
+++ BooklogExtension/booklog.js
@@ -12,37 +12,44 @@ for (var i=0;i<tagChildren.length;i+=1) {
 }
 
 // 今の棚
-$('img').each(function() {
-    if (String($(this).attr('src')).indexOf("ecx.images-amazon.com") !== -1) {
-        console.log($(this).attr('alt'));
-    }
-});
-
+var getNowThelf = function(tagHash) {
+    $('.shelfItemInfoTitle > a').each(function() {
+        //console.log($(this).text());
+        var titleStr = $(this).text();
+        var tagStr = tagHash[titleStr]
+        if (tagStr !== undefined) {
+            $(this).text(titleStr + ' [Tag:' + tagStr +']');
+        }
+    });
+}
 
 // 本の名前:タグ名
 var tagHash = [];
-
+var doAjax = function(nowTag) {
+        $.ajax({
+            // http://から始まるURLを指定
+            url: 'http://booklog.jp/users/' +userName +'/book?display=front&category_id=0&status=0&rank=0&tag=' + nowTag,
+            type: 'GET',
+            //asynchronous: true,
+            success: function(res) {
+                $(res).find('img').each(function() {
+                    if (String($(this).attr('src')).indexOf("ecx.images-amazon.com") !== -1) {
+                        // 本の名前:タグ名
+                        tagHash[$(this).attr('alt')] = nowTag;
+                    }
+                });
+                // タグを元に現在の棚に設定
+                getNowThelf(tagHash);
+            }
+        });
+}
 // スクレイピング
 $(document).ready(function() {
     // タグで回す
     for(var cnt in tagAry) {
-        console.log(tagAry[cnt]);
-        $.ajax({
-            // http://から始まるURLを指定
-            url: 'http://booklog.jp/users/' +userName +'/book?display=front&category_id=0&status=0&rank=0&tag=' + tagAry[cnt],
-            type: 'GET',
-            success: function(res) {
-                var bookStr = "";
-                $(res).find('img').each(function() {
-                    if (String($(this).attr('src')).indexOf("ecx.images-amazon.com") !== -1) {
-                        bookStr += $(this).attr('alt');
-                        // 本の名前:タグ名
-                        tagHash[bookStr] = $(this).attr('alt');
-                    }
-                });
-                console.log(bookStr);
-            }
-        });
+        var nowTag = tagAry[cnt];
+        console.log(nowTag);
+        doAjax(nowTag);
     }
 });
 
