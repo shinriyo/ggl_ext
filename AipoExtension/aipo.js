ae440dbc0195a1571e732e6c3968db43c914cd4c
shinriyo <shinriyo@gmail.com>
2012-12-05 09:28:16 +0900

    0.2 unity extension

--- AipoExtension/aipo.js
+++ AipoExtension/aipo.js
@@ -5,22 +5,17 @@ $(".cSelect li").click(function() {
 
 var language = localStorage['unity_language'] ? localStorage['unity_language'] : 'cs'; // default is C#
 
-$("pre.code[code_lang_name]").hide();
 switch (language) {
     case 'C#':
         $(".cSelect .cSelect-Selected").text("C#");
-        $("pre.code[code_lang_name='C#']").show();
         break;
     case 'Boo':
         $(".cSelect .cSelect-Selected").text("Boo");
-        $("pre.code[code_lang_name='Boo']").show();
         break;
     case 'JavaScript': // it is UnityScript
         $(".cSelect .cSelect-Selected").text("JavaScript");
-        $("pre.code[code_lang_name='JavaScript']").show();
         break;
     default:
         $(".cSelect .cSelect-Selected").text("C#");
-        $("pre.code[code_lang_name='C#']").show();
         break;
 }
